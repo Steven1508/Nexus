bcbb55c72bea1f512e427fc3672218800e897d01
Steven1508 <68759457+Steven1508@users.noreply.github.com>
2023-03-15 17:24:54 -0600

    Creacion del Proyecto

---
+++ src/test/java/com/Nexus/NexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
